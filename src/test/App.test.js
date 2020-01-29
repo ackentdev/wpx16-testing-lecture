81ba08e8f7b1284bad879967f5c664e3c2838325
ackentdev <adamkentdev@gmail.com>
2020-01-29 11:02:05 -0700

    app.test suite

---
+++ src/test/App.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import App from '../App';
+
+let container = null;
+
+beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+});
+
+afterEach(()=> {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+it('Renders todos', () => {
+    act(() => {
+        render(<App />, container)
+    })
+    console.log(container.textContent)
+    expect(container.textContent).toContain('Go to the store');
+    expect(container.textContent).toContain('Get drivers license');
+});
