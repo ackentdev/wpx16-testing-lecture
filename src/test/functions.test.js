85b607cd8ec54176a1fbc1e7fd9eb058f03759b2
ackentdev <adamkentdev@gmail.com>
2020-01-29 10:38:21 -0700

    first commit

---
+++ src/test/functions.test.js
@@ -0,0 +1,63 @@
+const {sum, sayHello, fetchUser} = require('../functions.js')
+
+test('adds 1 + 2 to equal 3', () =>{
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('sayHello says hello', () => {
+    expect(sayHello()).toBe('hello')
+});
+
+test('object assignment', () => {
+    const data = { one: 1 };
+    data['two'] = 2;
+    expect(data).toEqual({ one: 1, two: 2 })
+});
+
+test('Expect true to be truthy', () => {
+    expect(true).toBeTruthy()
+});
+
+let names = ['John', 'Jacob', 'Geoff'];
+
+test('name contains Geoff', () => {
+    expect(names).toContain('Geoff')
+});
+
+test("User name is Leanne", async () => {
+    expect.assertions(1);
+    const data = await fetchUser();
+    expect(data.name).toEqual('Leanne Graham');
+});
+
+let bankAccount = {
+    balance: 1000,
+    depositMoney(amount){
+        this.balance += amount
+    },
+    withdrawMoney(amount){
+        this.balance -= amount
+    }
+};
+
+describe("Bank account methods and properties", () => {
+    afterEach(() => {
+        bankAccount.balance = 1000
+    })
+
+    test('Initial balance is 1000', () => {
+        expect(bankAccount.balance).toBe(1000)
+    });
+
+    test('depositMoney should correctly alter balance', () => {
+        bankAccount.depositMoney(2000);
+        expect(bankAccount.balance).toBe(3000);
+    });
+
+    test('withDraw should correctly alter balance', () => {
+        bankAccount.withdrawMoney(2000);
+        expect(bankAccount.balance).toBe(-1000);
+    })
+})
+
+
